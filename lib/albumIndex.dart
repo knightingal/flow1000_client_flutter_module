b47ad66e33e16bb34859a57f150c3559e94420e0
knightingal <yujianming1985@hotmail.com>
2025-03-09 19:20:22 +0800

    load album list

--- lib/albumIndex.dart
+++ lib/albumIndex.dart
@@ -1,18 +1,19 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flow1000_admin/main.dart';
 import 'package:flow1000_admin/struct/album_info.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'config.dart';
+
 class AlbumIndexPage extends StatefulWidget{
+  const AlbumIndexPage({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return AlbumIndexState();
   }
-
-  
 }
 
 class AlbumIndexState extends State<AlbumIndexPage> {
@@ -27,16 +28,64 @@ class AlbumIndexState extends State<AlbumIndexPage> {
     }
   }
 
+  List<AlbumInfo> albumInfoList = [];
+
   @override
   void initState() {
     super.initState();
-    fetchAlbumIndex();
-
+    fetchAlbumIndex().then((albumInfoList) {
+      setState(() {
+        this.albumInfoList = albumInfoList;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Text("AlbumIndexPage");
+    Widget body;
+    if (albumInfoList.isEmpty) {
+      body = Text("AlbumIndexPage");
+    } else {
+      body = ListView.builder(
+        itemBuilder: (context, index) {
+          return DirItem(
+            index: index, 
+            title: albumInfoList[index].name, 
+            tapCallback: (int index, String title) {}
+          );
+        },
+        prototypeItem: DirItem(
+          index: 0, 
+          title: albumInfoList[0].name, tapCallback: (int index, String title) {}),
+        itemCount: albumInfoList.length,
+      );
+    }
+    return body;
   }
   
+}
+
+class DirItem extends StatelessWidget {
+  final String title;
+
+  final int index;
+  final void Function(int index, String title) tapCallback;
+
+  const DirItem({
+    super.key,
+    required this.index,
+    required this.title,
+    required this.tapCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        log("click $title");
+        tapCallback(index, title);
+      },
+      title: Text(title),
+    );
+  }
 }
