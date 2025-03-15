39014811fa969d54a3a081170b00f5c56f657d99
knightingal <yujianming1985@hotmail.com>
2025-03-15 12:47:13 +0800

    add encript_image

---
+++ lib/widget/encript_image.dart
@@ -0,0 +1,73 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../encrypt_lib.dart' as encrypt_lib;
+
+class EncriptImageWidget extends StatefulWidget {
+  const EncriptImageWidget({super.key, this.src, this.width, this.height});
+
+  final String? src;
+
+  final double? width;
+  final double? height;
+  
+
+  @override
+  State<StatefulWidget> createState() {
+    return EncriptImageState();
+  }
+}
+
+class EncriptImageState extends State<EncriptImageWidget> {
+  late Future<Uint8List> decriptedContentFuture;
+
+  Future<Uint8List> fetchImage() async {
+    final key = encrypt_lib.Key.fromUtf8("password");
+    final iv = encrypt_lib.IV.fromUtf8("");
+    final encrypter = encrypt_lib.Encrypter(
+        encrypt_lib.AES(key, mode: encrypt_lib.AESMode.cfb, padding: null));
+    String src = switch (widget.src) {
+      null => "http://192.168.2.12:3002/linux1000/encrypted/20151209003040BB-29 USS NORTH DAKOTA/011739.jpg.bin",
+      var s => s,
+    };
+    
+    final reps2Future =
+        // http.get(Uri.parse("http://192.168.2.12:8000/dev/aes-image"));
+        http.get(Uri.parse(src));
+    return reps2Future.then((resp2) {
+      if (resp2.statusCode == 200) {
+        Uint8List bytes = resp2.bodyBytes;
+        List<int> body =
+            encrypter.decryptBytes(encrypt_lib.Encrypted(bytes), iv: iv);
+        Uint8List i8list = Uint8List.fromList(body);
+        return i8list;
+      } else {
+        return Uint8List.fromList([]);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    decriptedContentFuture = fetchImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body = FutureBuilder<Uint8List>(
+        future: decriptedContentFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            return Image.memory(snapshot.data!, width: widget.width, height: widget.height,);
+          } else {
+            return const Text("");
+          }
+        });
+
+    return body;
+  }
+}
