005b9d8c5456f004790ead1d071008fe4b8fced0
knightingal <yujianming1985@hotmail.com>
2025-04-09 21:43:16 +0800

    add albumContentUrl()

--- lib/config.dart
+++ lib/config.dart
@@ -2,3 +2,4 @@
 String apiHost() => "http://192.168.2.12:8000"; 
 
 String albumIndexUrl() => "${apiHost()}/local1000/picIndexAjax";
+String albumContentUrl(int index) => "${apiHost()}/local1000/picDetailAjax?id=$index";
