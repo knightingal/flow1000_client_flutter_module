d1b2ed9de184daae9d37729a959b86780a91f238
knightingal <yujianming1985@hotmail.com>
2025-04-09 21:58:56 +0800

    switch to CustomScrollView in ablum_content

--- lib/album_content.dart
+++ lib/album_content.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:flow1000_admin/scroll.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'config.dart';
 import 'struct/album_info.dart';
+import 'struct/slot.dart';
 
 class AlbumContentPage extends StatefulWidget {
   const AlbumContentPage({super.key, required this.albumIndex});
@@ -20,6 +22,7 @@ class AlbumContentPage extends StatefulWidget {
 
 class AlbumContentPageState extends State<AlbumContentPage> {
 
+  late double width;
   Future<List<AlbumInfo>> fetchAlbumIndex() async {
     final response = await http.get(Uri.parse(albumContentUrl(widget.albumIndex)));
     if (response.statusCode == 200) {
@@ -33,39 +36,54 @@ class AlbumContentPageState extends State<AlbumContentPage> {
 
   late Future<List<AlbumInfo>> futureAblumList;
 
+  List<AlbumInfo> albumInfoList = [];
+  List<Slot> slot = [Slot()];
+
   @override
   void initState() {
     super.initState();
-    futureAblumList = fetchAlbumIndex();
+    fetchAlbumIndex().then((albumInfoList) {
+      for (int i = 0; i < albumInfoList.length; i++) {
+        AlbumInfo albumInfo = albumInfoList[i];
+        double coverWidth = width / slot.length;
+        double coverHeight = albumInfo.coverHeight * (coverWidth / albumInfo.coverWidth);
+        // log("coverHeight:$coverHeight, coverWidth:$coverWidth");
+        albumInfo.realHeight = coverHeight;
+        albumInfo.realWidth = coverWidth;
+
+        int slotIndex = minSlot(slot);
+        Slot slotOne = slot[slotIndex];
+        slotOne.slotItemList
+            .add(SlotItem(i, slotOne.totalHeight, coverHeight, slotIndex));
+        slotOne.totalHeight = slotOne.totalHeight + coverHeight;
+      }
+      setState(() {
+        this.albumInfoList = albumInfoList;
+      });
+    });
 
   }
 
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
     Widget body;
-    body = FutureBuilder<List<AlbumInfo>>(
-      future: futureAblumList, 
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          return LayoutBuilder(builder: (context, constraints) {
-            return GridView.builder(
-              itemCount: snapshot.data!.length,
-              gridDelegate: 
-                SliverGridDelegateWithFixedCrossAxisCount(
-                  childAspectRatio: 4 / 3, 
-                  crossAxisCount: 4
-                ), 
-              itemBuilder: (context, index) {
-                // return Text(snapshot.data![index].name);
-                return Image.network(snapshot.data![index].toCoverUrl());
-              });
-          });
-        } else {
-          return const Text("");
-        }
-      }
-    );
-
+    if (albumInfoList.isEmpty) {
+      body = Text("AlbumIndexPage");
+    } else {
+      body = CustomScrollViewExample(
+        slots: slot, 
+        builder: (BuildContext context, int index) {
+          return Image.network(
+            key: Key("content-$index"),
+            albumInfoList[index].toCoverUrl(), 
+            width: albumInfoList[index].realWidth, 
+            height: albumInfoList[index].realHeight,
+          );
+        }, 
+        totalLength: albumInfoList.length
+      );
+    }
     return body;
   }
   
