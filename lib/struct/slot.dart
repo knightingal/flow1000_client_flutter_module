5fcb30b4e4db07d5ca5dac70b371581d5887f89f
knightingal <yujianming1985@hotmail.com>
2025-04-22 21:25:32 +0800

    add SlotGroup class

--- lib/struct/slot.dart
+++ lib/struct/slot.dart
@@ -22,6 +22,56 @@ class Slot {
     return slotItemList.any((item) => item.index == index);
   }
 }
+
+class SlotGroup {
+  factory SlotGroup.fromCount(int slotCount) {
+    return SlotGroup(List.generate(slotCount, (_) => Slot(), growable: false));
+  }
+
+  final List<SlotItem> slotItemList = [];
+  final List<Slot> slots;
+
+  SlotGroup(this.slots);
+
+  void insertSlotItem(SlotItem slotItem) {
+    int slotIndex = minSlot();
+    Slot slotOne = slots[slotIndex];
+
+    slotItem.slotIndex = slotIndex;
+    slotItem.indexInSlot = slotItemList.length;
+    slotItem.scrollOffset = slotOne.totalHeight;
+
+    slotOne.slotItemList.add(
+      // SlotItem(i, slotOne.totalHeight, albumInfo.frameHeight, slotIndex),
+      slotItem,
+    );
+    slotOne.totalHeight = slotOne.totalHeight + slotItem.itemHeight;
+    slotItemList.add(slotItem);
+  }
+
+  double totalHeight() {
+    double max = 0;
+    for (int i = 0; i < slots.length; i++) {
+      if (slots[i].totalHeight > max) {
+        max = slots[i].totalHeight;
+      }
+    }
+    return max;
+  }
+
+  int minSlot() {
+    double min = slots[0].totalHeight;
+    int index = 0;
+    for (int i = 0; i < slots.length; i++) {
+      if (slots[i].totalHeight < min) {
+        min = slots[i].totalHeight;
+        index = i;
+      }
+    }
+    return index;
+  }
+}
+
 int minSlot(List<Slot> slot) {
   double min = slot[0].totalHeight;
   int index = 0;
