f410622045b68b4fbc357280dbf00ad9f2d88f6f
knightingal <yujianming1985@hotmail.com>
2025-04-22 21:52:17 +0800

    enhance build script

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,49 @@
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Properties
+import okhttp3.OkHttpClient
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+buildscript {
+    dependencies{
+
+        classpath("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+    }
+}
+
+var keystorePropertiesFile = rootProject.file("keystore.properties")
+var keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
 
 android {
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     namespace = "com.example.flow1000_admin"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -21,7 +59,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flow1000_admin"
+        applicationId = "org.nanking.flow1000_admin"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,7 +72,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -42,3 +80,52 @@ android {
 flutter {
     source = "../.."
 }
+fun String.execute(): Process {
+    val runtime = Runtime.getRuntime()
+    return runtime.exec(this)
+}
+
+fun Process.text(): String {
+    val inputStream = this.inputStream
+    val insReader = InputStreamReader(inputStream)
+    val bufReader = BufferedReader(insReader)
+    var output = ""
+    var line: String = ""
+    line = bufReader.readLine()
+    output += line
+    return output
+}
+
+
+fun releaseTime(): String = SimpleDateFormat("yyMMdd").format(Date())
+
+fun versionCode(): Int = SimpleDateFormat("yyMMdd0HH").format(Date()).toInt()
+//fun versionCode(): Int = 10
+
+fun commitNum(): String {
+    val resultArray = "git describe --always".execute().text().trim().split("-")
+    return resultArray[resultArray.size - 1]
+}
+
+
+task("releaseUpload") {
+    dependsOn("assembleRelease")
+    doLast {
+        println("do releaseUpload")
+        val target = "${project.buildDir}/outputs/apk/release/app-release.apk"
+        println(target)
+        val client:OkHttpClient = OkHttpClient().newBuilder().build();
+        val body = MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("file", target,
+                File(target).asRequestBody("application/octet-stream".toMediaTypeOrNull())
+            )
+            .build()
+        val request = Request.Builder()
+            .url("http://localhost:8000/apkConfig/upload")
+            .method("POST", body)
+            .build()
+        val response = client.newCall(request).execute()
+        println("${response.code.toString()}  ${response.body.string()}")
+    }
+}
+
