5cb45dc04bd51ea28376ccb23043b124ee12e578
knightingal <yujianming1985@hotmail.com>
2025-04-27 20:24:33 +0800

    add album param in albumIndexUrl

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,5 @@
 String apiHost() => "http://192.168.2.12:8000";
-String albumIndexUrl() => "${apiHost()}/local1000/picIndexAjax";
+String albumIndexUrl({String? album}) =>
+    "${apiHost()}/local1000/picIndexAjax?album=${album ?? ""}";
 String albumContentUrl(int index) =>
     "${apiHost()}/local1000/picDetailAjax?id=$index";
