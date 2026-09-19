420c5d27c1da0f19efa48776c299c85985e795dd
knightingal <yujianming1985@hotmail.com>
2026-09-19 20:07:29 +0800

    fix compile error

--- lib/album_index.dart
+++ lib/album_index.dart
@@ -34,7 +34,7 @@ class AlbumIndexState extends State<AlbumIndexPage> {
     String rootPath = "${directory!.path}${Platform.pathSeparator}Download";
 
     List<AlbumInfo> albumInfoList = imgRow
-        .map((e) => AlbumInfo.fromJson(e, rootPath))
+        .map((e) => AlbumInfo.fromMap(e, rootPath))
         .toList();
     return albumInfoList;
   }
